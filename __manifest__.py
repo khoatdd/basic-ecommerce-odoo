6f5bfa510f56a4934dc7d4da41f2aad423529cf8
legenddotavn <dangkhoa114@gmail.com>
2017-09-13 19:56:28 +0700

    Create 'Restricted Menus Group' and 'Administrator' group, Hide menus

--- __manifest__.py
+++ __manifest__.py
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -32,4 +33,7 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'auto_install': True,
+    'installable': True,
+    'application': True,
 }
